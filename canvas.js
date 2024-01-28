60e108c58aba0327be0f6b2c281bb45b58cfa9ec
flokleiser <132885161+flokleiser@users.noreply.github.com>
2024-01-28 15:47:43 +0100

    this is sucks

--- canvas.js
+++ canvas.js
@@ -1,36 +1,41 @@
-// var canvas = document.querySelector('canvas');
-// var ctx = canvas.getContext('2d');
+var canvas = document.querySelector('canvas');
+var ctx = canvas.getContext('2d');
 
-// var image1 = new Image();
-// image1.src = "images/test_day.png"
+var image1 = new Image();
+image1.src = "images/test_day.png"
 
-// var image2 = new Image();
-// image2.src = "images/test_clouds.png"
+var image2 = new Image();
+image2.src = "images/test_clouds.png"
 
 // var currentOpacity = 1.0;
-// var increment = 0.01;
+var currentOpacity = 0.5;
+var increment = 0.01;
 
-// image1.onload = function() {
-//   canvas.width = this.naturalWidth;
-//   canvas.height = this.naturalHeight; 
-//   drawImages;
-// };
+image1.onload = 
+  drawImages;
 
-// image2.onload = function() {
-//   canvas.width = this.naturalWidth;
-//   canvas.height = this.naturalHeight; 
-//   drawImages;
-// };
 
-// function drawImages() {
-//   ctx.clearRect(0,0,canvas.width, canvas.height);
+image2.onload = 
+  drawImages;
 
-//   ctx.globalAlpha = currentOpacity;
-//   ctx.drawImage(image1,0,0);
-//   ctx.globalAlpha = 1.0 - currentOpacity;
+function drawImages() {
+  canvas.width = this.naturalWidth;
+  canvas.height = this.naturalHeight; 
 
-//   ctx.drawImage(image2,0,0);
-//   currentOpacity -= increment;
+  // ctx.globalAlpha = currentOpacity;
+  ctx.globalAlpha = 0.1;
+
+  // ctx.drawImage(image1,0,0,image1.width,image1.height);
+  ctx.drawImage(image2,0,0,image2.width,image2.height);
+
+  // ctx.clearRect(0,0,canvas.width, canvas.height);
+
+  
+  // ctx.drawImage(image1,0,0);
+  // ctx.globalAlpha = 1.0 - currentOpacity;
+
+  // ctx.drawImage(image2,0,0);
+  // currentOpacity -= increment;
 
 //   if (currentOpacity <= 0) {
 //     var temp = image1;
@@ -39,20 +44,20 @@
 //     currentOpacity = 1.0;
 //   }
 // requestAnimationFrame(drawImages);
-// }
-
-
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
-
-var image = new Image();
-image.src = "images/test_day.png"
-image.onload = drawImageSize;
-
-function drawImageSize() {
-  canvas.width = this.naturalWidth;
-  canvas.height = this.naturalHeight;
-
-  ctx.drawImage(this, 0, 0);
-  ctx.drawImage(this, 0, 0, this.width, this.height);
 }
+
+
+// var canvas = document.querySelector('canvas');
+// var ctx = canvas.getContext('2d');
+
+// var image = new Image();
+// image.src = "images/test_day.png"
+// image.onload = drawImageSize;
+
+// function drawImageSize() {
+//   canvas.width = this.naturalWidth;
+//   canvas.height = this.naturalHeight;
+
+//   ctx.drawImage(this, 0, 0);
+//   ctx.drawImage(this, 0, 0, this.width, this.height);
+// }
